082e96d003e63dfa34923ebf7db294964f319a4f
Lena Gavrilova <lena.gavrilova2408@mail.ru>
2022-05-06 12:11:18 +0300

    done tasks

--- src/quick_sort.cpp
+++ src/quick_sort.cpp
@@ -18,10 +18,13 @@ namespace assignment {
     }
 
     // вычисляем индекс опорного элемента ... median_of_three ...
-    int pivot_index = -1 /* напишите здесь свой код ... */;
+    int middle = median_of_three(arr,start,stop);
 
     // производим разбиение относительно опорного элемента ...  partition ...
+    int pivot_index= partition(arr,start,stop,middle);
     // рекурсивно повторяем процедуру над левой и правой частью ...
+    quick_sort(arr,start,pivot_index-1);
+    quick_sort(arr,pivot_index+1,stop);
   }
 
 }  // namespace assignment
